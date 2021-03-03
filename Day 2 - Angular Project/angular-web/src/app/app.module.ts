cc3ee3a2215a4e61961ee9734d06f224e975417f
Nikesh Ghimire <Nikesh.Ghimire@bp.com>
2021-03-02 21:53:15 -0600

    Basic layout using angular with lazy loading routes

---
+++ Day 2 - Angular Project/angular-web/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Day 2 - Angular Project/angular-web/src/app/content/products/product.module.ts
@@ -0,0 +1,14 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProductsComponent} from './products.component';
+import {ProductRoutingModule} from './product-routing.module';
+
+@NgModule({
+  declarations: [ProductsComponent],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
